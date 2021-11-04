bf2b15c6e73dd7a2a01f03995526e1c9ff0fae66
Andrea Vrljic <andrea31542@gmail.com>
2021-11-04 18:49:41 +0100

    Add UserControl to main form and implement evaluation via button

--- SquareEquation/QuadraticEquation.cs
+++ SquareEquation/QuadraticEquation.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace SquareEquation
 {
-    class QuadraticEquation
+    public class QuadraticEquation
     {
         // Hide element
         private double a;
